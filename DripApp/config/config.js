d003e6b7b195adc8bedbfb8caa849fe2fd57e1e7
Aziza Jurabaeva <azizajurabaeva@gmail.com>
2019-11-04 16:59:48 -0500

    initial commit

---
+++ DripApp/config/config.js
@@ -0,0 +1,22 @@
+//conecting app to firebase - adding database, auth, storage
+import firebase from 'firebase';
+
+//api detains
+const config = {
+    apiKey: "AIzaSyAxY48vuJO6gbxEMVQcx4p88s-1OQ4lL9k",
+    authDomain: "drip-1d5c2.firebaseapp.com",
+    databaseURL: "https://drip-1d5c2.firebaseio.com",
+    projectId: "drip-1d5c2",
+    storageBucket: "drip-1d5c2.appspot.com",
+    messagingSenderId: "688706937662",
+    appId: "1:688706937662:web:1c9c28cdefd9a112bbc26f",
+    measurementId: "G-T6BBGMSHX3"
+  };
+
+firebase.initializeApp(config);
+export const f = firebase;
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const storage = firebase.storage();  
+
+
